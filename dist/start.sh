3787cb287b628641d913f7ec966bd8beb8512c48
Flavio Fernandes <flavio@flaviof.com>
2022-12-23 17:08:06 -0500

    Add systemd service
    As a reference for what one would do to make this project start.
    And as what flaviof uses for its iot home automation. :)

--- dist/start.sh
+++ dist/start.sh
@@ -7,8 +7,9 @@ cd "$(dirname $0)"
 
 # https://stackoverflow.com/questions/3174883/how-to-remove-last-directory-from-a-path-with-sed
 export TOPDIR="${PWD%/*}"
-export LOGDIR="${LOGDIR:-${TOPDIR}}/log"
+export PROG=${PROG:-${TOPDIR}/cmd/mqtt2ping/mqtt2ping}
+export LOGDIR=${LOGDIR:-${TOPDIR}/log}
 export CONFIG=${CONFIG:-${TOPDIR}/data/config_ff.yaml}
 export BROKERURL=${BROKERURL:-"tcp://192.168.10.238:1883"}
 
-./mqtt2ping -verbose
+sudo ${PROG} -broker ${BROKERURL} -config ${CONFIG} -logdir ${LOGDIR} -verbose
